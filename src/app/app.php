3f1b35d3d5b420bec4ee59a782fc8ff8f5ce2883
Micha de Vries <mt.dev@hotmail.com>
2018-11-14 13:36:21 +0100

    made a core file for interal plugins

--- src/app/app.php
+++ src/app/app.php
@@ -1,8 +1,9 @@
 <?php
   if (file_exists(__DIR__ . '/classes.php')) {
     require_once __DIR__ . '/classes.php';
+    if (file_exists(__DIR__ . '/internal/core.php')) { require_once __DIR__ . '/internal/core.php'; } else { echo  'Whoops, an internal error occured, this page will soon be more detailed and styled'; die; }
     $app = new App;
-    
+
     //script your code for your custom plugins in here...
 
   } else {
